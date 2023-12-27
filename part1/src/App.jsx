2817fdb43aa3d15df48b20df2d67091d938fa375
LucianoAndresLugani <63163767+LucianoAndresLugani@users.noreply.github.com>
2023-12-27 00:25:23 -0300

    first commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,53 @@
+
+const Header = (props) => {
+  return (
+
+    <>
+     <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+
+    <>
+   <p>{props.part}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+
+    <>
+   <p>{props.exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} />
+      <Total exercises={exercises1} />
+      <Header course={course} />
+      <Content part={part2} />
+      <Total exercises={exercises2} />
+      <Header course={course} />
+      <Content part={part3} />
+      <Total exercises={exercises3} />
+    </div>
+  )
+}
+export default App
