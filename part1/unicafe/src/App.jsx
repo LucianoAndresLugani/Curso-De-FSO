8e22a9499468ec7a591897a626ac22380cd53841
LucianoAndresLugani <63163767+LucianoAndresLugani@users.noreply.github.com>
2023-12-30 01:59:54 -0300

    1.7: unicafe, paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,28 +3,60 @@ const Display = props => <div><span>{props.op}</span><p>{props.value}</p></div>
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll]= useState(0);
+  const [average, setAverage]= useState(0)
+  const [positive, setPositive]= useState(0);
+
+  const setToGood = (newValue) => {
+    setAll(all+1);
+    setToAverage();
+    setToPositive();
+    setGood(newValue)
+  }
+  const setToNeutral = (newValue) => {
+    setAll(all+1)
+    setToAverage();
+    setToPositive();
+    setNeutral(newValue);
+  }
+  const setToBad = (newValue) => {
+    setAll(all+1);
+    setToPositive();
+    setToAverage();
+    setBad(newValue);
+  }
+  const setToAverage = () => {
+    let avg = (good - bad)/all;
+    setAverage(avg);
+  }
+  const setToPositive=()=> {
+    let pos = (100*good)/all;
+    setPositive(pos);
+  }
 
 
-  
   return (
     <div>
        <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
+      <button onClick={() => setToGood(good + 1)}>
         GOOD
       </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
+      <button onClick={() => setToNeutral(neutral + 1)}>
         NEUTRAL
       </button>
-      <button onClick={() => setBad(bad + 1)}>
+      <button onClick={() => setToBad(bad + 1)}>
         BAD
       </button>
       <h1>Stadistics</h1>
       <Display value={good} op={"GOOD"}>Good</Display>
       <Display value={neutral} op={"NEUTRAL"}>Neutral</Display>
       <Display value={bad} op={"BAD"}>Bad</Display>
+      <Display value={all} op={"ALL"}>ALL</Display>
+      <Display value={average} op={"AVERAGE"}>Average</Display>
+      <Display value={positive+"%"} op={"POSOTIVE"}>Average</Display>
     </div>
   )
 }
